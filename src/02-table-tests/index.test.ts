ef50ed8793416605cfded5e93e584d8713ba7c3c
batar-btr <btrn86@gmail.com>
2023-07-03 20:24:51 +0300

    feat: comment some test cases

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -6,20 +6,21 @@ const testCases = [
   { a: 2, b: 2, action: Action.Add, expected: 4 },
   { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 1, b: 2, action: Action.Multiply, expected: 2 },
-  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
-  { a: 4, b: 2, action: Action.Multiply, expected: 8 },
+  // { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  // { a: 4, b: 2, action: Action.Multiply, expected: 8 },
   { a: 2, b: 2, action: Action.Divide, expected: 1 },
-  { a: 10, b: 2, action: Action.Divide, expected: 5 },
-  { a: 14, b: 2, action: Action.Divide, expected: 7 },
+  // { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  // { a: 14, b: 2, action: Action.Divide, expected: 7 },
   { a: 10, b: 2, action: Action.Exponentiate, expected: 100 },
-  { a: 10, b: 3, action: Action.Exponentiate, expected: 1000 },
-  { a: 10, b: 4, action: Action.Exponentiate, expected: 10000 },
+  // { a: 10, b: 3, action: Action.Exponentiate, expected: 1000 },
+  // { a: 10, b: 4, action: Action.Exponentiate, expected: 10000 },
   { a: 10, b: 2, action: Action.Subtract, expected: 8 },
-  { a: 10, b: 3, action: Action.Subtract, expected: 7 },
-  { a: 10, b: 4, action: Action.Subtract, expected: 6 },
-  { a: 'incorrect', b: 4, action: Action.Subtract, expected: null },
-  { a: 10, b: 4, action: 'incorrect', expected: null },
+  // { a: 10, b: 3, action: Action.Subtract, expected: 7 },
+  // { a: 10, b: 4, action: Action.Subtract, expected: 6 },
+  // { a: 'incorrect', b: 4, action: Action.Subtract, expected: null },
+  // { a: 10, b: 4, action: 'incorrect', expected: null },
 ];
+// Uncomment to run more test cases!)
 
 describe('simpleCalculator', () => {
   test.each(testCases)(
